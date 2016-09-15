a08d4c43ad9b771fa288a3d5ac968374712fb27f
Guang Yan <2567268970@qq.com>
2016-09-15 19:02:49 +0800

    Debug Version
    Excute success

---
+++ Classes/HelloScene.h
@@ -0,0 +1,24 @@
+﻿#ifndef __HELLO_SCENE_H__
+#define __HELLO_SCENE_H__
+
+#include "cocos2d.h"
+
+#include "MainScene.h"
+
+class Hello : public cocos2d::Layer
+{
+public:
+    static cocos2d::Scene* createScene();
+
+    virtual bool init();
+    
+    // a selector callback
+    void menuCloseCallback(cocos2d::Ref* pSender);
+    
+    // implement the "static create()" method manually
+    CREATE_FUNC(Hello);
+
+	void nextScene(float dt);
+};
+
+#endif // __HELLO_SCENE_H__
---
+++ Classes/MyGameScene.h
@@ -0,0 +1,40 @@
+﻿#ifndef __MYGAME_SCENE_H__
+#define __MYGAME_SCENE_H__
+
+#include "cocos2d.h"
+#include "SimpleAudioEngine.h"
+#include "HRocker.h"
+#include "Hero.h"
+
+class MyGame : public cocos2d::Layer
+{
+	Layer *gameView;//将_player和_tileMap添加到这一个节点中，方便滚动地图时同时改变位置，同时摇杆位置不动
+	HRocker *rocker;
+	cocos2d::TMXTiledMap* _tileMap;
+	cocos2d::TMXLayer* _collidable;
+	cocos2d::TMXLayer* _end;
+	cocos2d::Sprite *_player;
+	Hero *hero;
+public:
+	static cocos2d::Scene* createScene();
+
+	virtual bool init();
+	//判断是否到达终点
+	bool isEnded(Vec2 pos);
+	virtual void update(float delta);
+
+	void setPlayerPosition(cocos2d::Vec2 position);
+	cocos2d::Vec2 tileCoordFromPosition(cocos2d::Vec2 position);
+	void setViewpointCenter(cocos2d::Vec2 position);
+
+	void menuCloseCallback(Ref * pSender);
+
+	// a selector callback
+
+
+	// implement the "static create()" method manually
+	CREATE_FUNC(MyGame);
+};
+
+#endif // __MYGAME_SCENE_H__
+
